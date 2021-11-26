53c22f35ddba60587963e5ee32e898c5dea8dc85
RyenBZhao <fouxzhao@GMAIL.COM>
2021-11-26 01:39:19 +0000

    Week2-4-0 Self Test

--- TrainingFiles/C2_3.asm
+++ TrainingFiles/C2_3.asm
@@ -1,26 +1,36 @@
 .text
-    addi $s0, $zero, 4        # Loading s0 with an example value, 4 (so it should prompt "neither!")
-    addi $v0, $zero, 4        # syscall code for printing a string (will be reused throughout)
+    la   $a0, MSG_INPUT_PROMPT
+    addi $v0, $zero, 4        # to print string
+    syscall                   # Prompt to get an input
+
+    addi $v0, $zero, 5        # to get integer
+    syscall
+    add  $s0, $zero, $v0      # Store the input value into s0
+
+    addi $v0, $zero, 4        # to print string
     addi $t0, $zero, 1
-    la   $a0, MSG_ONE
-    beq  $s0, $t0,   IS_ONE   # Print MSG_ONE
-    beq  $s0, $zero, IS_ZERO  # Print MSG_ZERO
-    la $a0, MSG_NEITHER       # Print MSG_NEITHER
+    la   $a0, MSG_NEITHER     # load MSG_NEITHER
+    beq  $s0, $t0,   IS_ONE   # if input equals 1, load MSG_ONE
+    beq  $s0, $zero, IS_ZERO  # if input equals 0, load MSG_ZERO
+    syscall                   # print MSG of 1or0 judgement
+    la   $a0, MSG_Num_Stroge
+    syscall                   # print "Your Input: "
+    addi $v0, $zero, 1        # to print integer
+    add  $a0, $zero, $s0      # print the value of Input
     syscall
     j EXIT
   IS_ZERO:
     la   $a0, MSG_ZERO
-    syscall
-    j EXIT
   IS_ONE:
-    syscall
-    j EXIT
+    la   $a0, MSG_ONE
   EXIT:
     addi $v0, $zero, 10
     syscall
 
-# the memory structure comes after the .data directive
+
 .data
-  MSG_ZERO:     .asciiz     "it was zero!"
-  MSG_ONE:      .asciiz     "it was one!"
-  MSG_NEITHER:  .asciiz     "it was neither!"
+  MSG_Num_Stroge:   .asciiz     "Your Input: "
+  MSG_INPUT_PROMPT: .asciiz     "Input: "
+  MSG_ZERO:         .asciiz     "\nIt was zero!"
+  MSG_ONE:          .asciiz     "\nIt was one!"
+  MSG_NEITHER:      .asciiz     "\nIt was neither!"
