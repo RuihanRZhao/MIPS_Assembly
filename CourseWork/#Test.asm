133c4cbb32b9b68fdfe2f3d6353b50fa3a1a0b39
RyenBZhao <fouxzhao@GMAIL.COM>
2021-12-08 05:17:33 +0000

    test 1.4

--- CourseWork/#Test.asm
+++ CourseWork/#Test.asm
@@ -28,15 +28,35 @@ la      $t0, buffer_for_input_string
 la      $t1, buffer_for_processed_string
 Loop:
 
-lb      $t2, 0($t0)
-add      $t2, $t2, -32
+lb      $t2,  0($t0)
+addi    $t3,  $t2,  0
+
+beq     $t2,  10,    SKIP
+
+li      $t7,  'A'
+slt     $t2,  $t7,   $t4
+li      $t7,  'Z'
+slt     $t7,  $t2,   $t5
+or      $t4,  $t5,  $t4
+beq     $t4, $zero, ACT
+jal     SKIP        # jump to SKIP and save position to $ra
+
+ACT:
+addi    $t3,  $t3,    32
+jal    SKIP        # jump to SKIP and save position to $ra
+
+
 # potentially do some processing on the character loaded in t2
-sb      $t2, 0($t1)
+SKIP:
+
+sb      $t3, 0($t1)
 addi    $t0, $t0, 1
 addi    $t1, $t1, 1
 bne     $t2, $zero, Loop # keep going until you reach the end of the string,
                          # which is demarcated by the null character.
+jal    End        # jump to End and save position to $ra
 
+End:
 # <<<< MAKE YOUR CHANGES ABOVE HERE
 
 # prompting the user with a message for the processed output:
